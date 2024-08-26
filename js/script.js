abcc9290431a7760d6226912eecb172b510ccbd3
Blu Corbel <blucorbel29@gmail.com>
2024-08-26 16:56:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,55 @@ document.addEventListener("DOMContentLoaded", function() {
     let startX;
     let scrollLeft;
 
-    carouselWrapper.addEventListener("mousedown", (e) => {
+    function handleMouseDown(e) {
         isDown = true;
         carouselWrapper.classList.add("active");
         startX = e.pageX - carouselWrapper.offsetLeft;
         scrollLeft = carouselWrapper.scrollLeft;
-    });
+    }
 
-    carouselWrapper.addEventListener("mouseleave", () => {
+    function handleMouseLeave() {
         isDown = false;
         carouselWrapper.classList.remove("active");
-    });
+    }
 
-    carouselWrapper.addEventListener("mouseup", () => {
+    function handleMouseUp() {
         isDown = false;
         carouselWrapper.classList.remove("active");
-    });
+    }
 
-    carouselWrapper.addEventListener("mousemove", (e) => {
+    function handleMouseMove(e) {
         if (!isDown) return;
         e.preventDefault();
         const x = e.pageX - carouselWrapper.offsetLeft;
         const walk = (x - startX) * 3;
         carouselWrapper.scrollLeft = scrollLeft - walk;
-    });
+    }
+
+    function handleTouchStart(e) {
+        isDown = true;
+        startX = e.touches[0].pageX - carouselWrapper.offsetLeft;
+        scrollLeft = carouselWrapper.scrollLeft;
+    }
+
+    function handleTouchEnd() {
+        isDown = false;
+    }
+
+    function handleTouchMove(e) {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.touches[0].pageX - carouselWrapper.offsetLeft;
+        const walk = (x - startX) * 3;
+        carouselWrapper.scrollLeft = scrollLeft - walk;
+    }
+
+    carouselWrapper.addEventListener("mousedown", handleMouseDown);
+    carouselWrapper.addEventListener("mouseleave", handleMouseLeave);
+    carouselWrapper.addEventListener("mouseup", handleMouseUp);
+    carouselWrapper.addEventListener("mousemove", handleMouseMove);
+    
+    carouselWrapper.addEventListener("touchstart", handleTouchStart);
+    carouselWrapper.addEventListener("touchend", handleTouchEnd);
+    carouselWrapper.addEventListener("touchmove", handleTouchMove);
 });
