d5887a444db078de296e923dc58cb0be7f9dd289
Blu Corbel <blucorbel29@gmail.com>
2024-08-28 18:13:17 +0100

    Update create-payment-intent.js

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,29 +1,31 @@
-// ./netlify/functions/create-payment-intent.js
+// netlify/functions/create-payment-intent.js
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async function(event, context) {
-    if (event.httpMethod === 'POST') {
-        try {
-            const { amount } = JSON.parse(event.body); // Adjust amount based on your needs
-            const paymentIntent = await stripe.paymentIntents.create({
-                amount,
-                currency: 'gbp',
-                payment_method_types: ['card'],
-            });
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-            };
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: error.message }),
-            };
-        }
-    } else {
+    if (event.httpMethod !== 'POST') {
         return {
             statusCode: 405,
-            body: JSON.stringify({ error: 'Method not allowed' }),
+            body: JSON.stringify({ error: 'Method Not Allowed' })
+        };
+    }
+
+    try {
+        const { amount } = JSON.parse(event.body);
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd'
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ clientSecret: paymentIntent.client_secret })
+        };
+    } catch (error) {
+        console.error('Error creating payment intent:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' })
         };
     }
 };
